abae6310706ef2cbf061317e69bef9d7b3cd41f5
Tony Lam <tony.lam@iki.fi>
2024-11-26 21:40:21 +0200

    refactor: migrate todistusvalinta_dataset from JS to JSON format and update import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { LineChart } from '@mui/x-charts/LineChart';
 import { mangoFusionPalette } from '@mui/x-charts/colorPalettes';
 
-import { todistusvalinta_dataset } from '././data/todistusvalinta-data.js';
+import { todistusvalinta_dataset } from '././data/todistusvalinta-data.json';
 import './App.css';
 
 import TodistusvalintaChart from './components/todistusvalinta.jsx';
