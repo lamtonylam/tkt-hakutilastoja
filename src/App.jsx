ca234956dbc9ba052031b2ba72ec30ec46af7324
Tony Lam <tony.lam@iki.fi>
2024-11-26 21:17:37 +0200

    refactor: chart component into TodistusvalintaChart for better modularity

--- src/App.jsx
+++ src/App.jsx
@@ -4,66 +4,15 @@ import { mangoFusionPalette } from '@mui/x-charts/colorPalettes';
 import { todistusvalinta_dataset } from './todistusvalinta-data.js';
 import './App.css';
 
+import TodistusvalintaChart from './components/todistusvalinta.jsx';
+
 function App() {
   return (
     <div>
       <div className='chart'>
         <h3>TKT-todistusvalinta ensikertalaisille hakijoille</h3>
-        <LineChart
-          colors={mangoFusionPalette}
-          margin={{
-            left: 80,
-            right: 80,
-            top: 140,
-            bottom: 80,
-          }}
-          dataset={todistusvalinta_dataset}
-          xAxis={[
-            {
-              dataKey: 'year',
-              valueFormatter: (value) => value.toString(),
-              scaleType: 'point',
-              label: 'Vuosi',
-            },
-          ]}
-          series={[
-            {
-              dataKey: 'helsinki',
-              label: 'Helsingin yliopisto',
-              valueFormatter: (value) => `${value}`,
-            },
-            {
-              dataKey: 'tampere',
-              label: 'Tampereen yliopisto',
-              valueFormatter: (value) => `${value}`,
-            },
-            {
-              dataKey: 'turku',
-              label: 'Turun yliopisto',
-              valueFormatter: (value) => `${value}`,
-            },
-            {
-              dataKey: 'oulu',
-              label: 'Oulun yliopisto',
-              valueFormatter: (value) => `${value}`,
-            },
-            {
-              dataKey: 'jyväskylä',
-              label: 'Jyväskylän yliopisto',
-              valueFormatter: (value) => `${value}`,
-            },
-            {
-              dataKey: 'joensuu',
-              label: 'Joensuu - Itä-Suomen yliopisto',
-              valueFormatter: (value) => `${value}`,
-            },
-            {
-              dataKey: 'kuopio',
-              label: 'Kuopio - Itä-Suomen yliopisto',
-              valueFormatter: (value) => `${value}`,
-            },
-          ]}
-          yAxis={[{ min: 55, max: 95, label: 'Pisteraja' }]}
+        <TodistusvalintaChart
+          todistusvalinta_dataset={todistusvalinta_dataset}
         />
       </div>
       <br />
---
+++ src/components/todistusvalinta.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { LineChart } from '@mui/x-charts';
+import { mangoFusionPalette } from '@mui/x-charts/colorPalettes';
+
+const TodistusvalintaChart = ({ todistusvalinta_dataset }) => {
+  return (
+    <LineChart
+      colors={mangoFusionPalette}
+      margin={{
+        left: 80,
+        right: 80,
+        top: 140,
+        bottom: 80,
+      }}
+      dataset={todistusvalinta_dataset}
+      xAxis={[
+        {
+          dataKey: 'year',
+          valueFormatter: (value) => value.toString(),
+          scaleType: 'point',
+          label: 'Vuosi',
+        },
+      ]}
+      series={[
+        {
+          dataKey: 'helsinki',
+          label: 'Helsingin yliopisto',
+          valueFormatter: (value) => `${value}`,
+        },
+        {
+          dataKey: 'tampere',
+          label: 'Tampereen yliopisto',
+          valueFormatter: (value) => `${value}`,
+        },
+        {
+          dataKey: 'turku',
+          label: 'Turun yliopisto',
+          valueFormatter: (value) => `${value}`,
+        },
+        {
+          dataKey: 'oulu',
+          label: 'Oulun yliopisto',
+          valueFormatter: (value) => `${value}`,
+        },
+        {
+          dataKey: 'jyväskylä',
+          label: 'Jyväskylän yliopisto',
+          valueFormatter: (value) => `${value}`,
+        },
+        {
+          dataKey: 'joensuu',
+          label: 'Joensuu - Itä-Suomen yliopisto',
+          valueFormatter: (value) => `${value}`,
+        },
+        {
+          dataKey: 'kuopio',
+          label: 'Kuopio - Itä-Suomen yliopisto',
+          valueFormatter: (value) => `${value}`,
+        },
+      ]}
+      yAxis={[{ min: 55, max: 95, label: 'Pisteraja' }]}
+    />
+  );
+};
+
+export default TodistusvalintaChart;
